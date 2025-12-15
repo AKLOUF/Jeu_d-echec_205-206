0c20ac593341895b8776eb91e0895dc0afdbcbc2
Tanim <113085404+tanim-veer@users.noreply.github.com>
2025-12-15 15:02:52 +0100

    modif piece

--- src/chess/businessLayer/piece/Piece.java
+++ src/chess/businessLayer/piece/Piece.java
@@ -1,10 +1,14 @@
-package chess.businesslayer.piece;
+package chess.businessLayer.piece;
 
 import chess.businesslayer.Color;
 import chess.businesslayer.IPiece;
+import chess.businessLayer.board.Board;
+import chess.businessLayer.board.Position;
 import chess.businesslayer.board.Square;
+import chess.businesslayer.piece.PieceType;
+import java.util.List;
 
-public class Piece implements IPiece {
+public abstract class Piece implements IPiece {
 
     private final Color color;
     private Square carre;
@@ -33,4 +37,7 @@ public class Piece implements IPiece {
     public void setSquare(Square square) {
         this.carre = square;
     }
+
+    // C'est ici que la magie opère : chaque pièce devra définir comment elle bouge
+    public abstract List<Position> getPseudoLegalMoves(Position currentPos, Board board);
 }

1d93bd3ce88d0f832046391773ee6d01acd1ccf8
Tanim <113085404+tanim-veer@users.noreply.github.com>
2025-12-20 12:15:00 +0100

    update position

--- src/chess/businessLayer/board/Position.java
+++ src/chess/businessLayer/board/Position.java
@@ -1,25 +1,12 @@
-package chess.businesslayer.board;
+package chess.businessLayer.board;
 
-import java.util.Objects;
-
-public final class Position {
-    public final int x, y;
-
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+public record Position(int x, int y) {
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Position)) return false;
         Position p = (Position) o;
-        return x == p.x && y == p.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
+        return x == p.x() && y == p.y();
     }
 }

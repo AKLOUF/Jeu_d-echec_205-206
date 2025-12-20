6d1694970a0feb9191af9e33984781fc90befce2
Tanim <113085404+tanim-veer@users.noreply.github.com>
2025-12-20 12:14:43 +0100

    update move

--- src/chess/businessLayer/Move.java
+++ src/chess/businessLayer/Move.java
@@ -2,30 +2,15 @@ package chess.businessLayer;
 
 import chess.businessLayer.board.Position;
 
-public class Move {
-    private final Position start;
-    private final Position end;
-
-    public Move(Position start, Position end){
-        this.end = end;
-        this.start = start;
-    }
-
-    public Position getStart(){
-        return start;
-    }
-
-    public Position getEnd(){
-        return end;
-    }
+public record Move(Position start, Position end) {
 
     public String toUCI() {
         return toAlg(start) + toAlg(end);
     }
 
     private String toAlg(Position p) {
-        char col = (char)('a' + p.x);
-        char row = (char)('1' + p.y);
+        char col = (char) ('a' + p.x());
+        char row = (char) ('1' + p.y());
         return "" + col + row;
     }
 }

36c6f5d7a98d4be2a5c37f7bf9b67facc5d42f95
AKLOUF <imad-eddine.aklouf@etu.u-paris.fr>
2025-12-20 13:19:33 +0100

    Clean up Piece class imports and properties
    Refactor imports and remove unused Square reference.

--- src/chess/businessLayer/piece/Piece.java
+++ src/chess/businessLayer/piece/Piece.java
@@ -1,17 +1,12 @@
-package chess.businessLayer.piece;
-
-import chess.businesslayer.Color;
-import chess.businesslayer.IPiece;
+import chess.businessLayer.Color;
+import chess.businessLayer.IPiece;
 import chess.businessLayer.board.Board;
 import chess.businessLayer.board.Position;
-import chess.businesslayer.board.Square;
-import chess.businesslayer.piece.PieceType;
 import java.util.List;
 
 public abstract class Piece implements IPiece {
 
     private final Color color;
-    private Square carre;
     private final PieceType type;
 
     public Piece(Color color, PieceType type) {
@@ -24,20 +19,10 @@ public abstract class Piece implements IPiece {
         return color;
     }
 
-    public Square getSquare() {
-        return carre;
-    }
-
     @Override
     public PieceType getType(){
         return type;
     }
 
-    @Override
-    public void setSquare(Square square) {
-        this.carre = square;
-    }
-
-    // C'est ici que la magie opère : chaque pièce devra définir comment elle bouge
     public abstract List<Position> getPseudoLegalMoves(Position currentPos, Board board);
 }
